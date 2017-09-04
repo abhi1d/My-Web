9b16614efa19075734bfca60626cb5d53cf72894
Abhishek sharma <abhiislikeme@gmail.com>
2017-09-04 23:01:45 +0530

    Update myJS.js
    On Click Button Not handled

--- myJS.js
+++ myJS.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-  $("img").addClass("animated fadeIn");
+  $("img").addClass("animated shake");
+  $("button").click(function(){
+    window.open('https://mail.google.com/mail/');
+});
    function sendMail() {
+   window.open('https://mail.google.com/mail/');
     var link = 'mailto:5h1dabhi@gmail.com?subject=Message from '
              +document.getElementById('senderEmail').value
              +'&body='+document.getElementById('message').value;
     window.location.href = link;
 }
+
 });
